3fb804d4db3a637cc11433774aa20c81244627ae
Assaf Stone <assaf.stone@microsoft.com>
2018-02-16 07:35:58 -0500

    fixed publih to wiki - inputs, fetch rather than clone

--- Extensions/PublishToWiki/PublishToWikiTask/PublishToWiki.ps1
+++ Extensions/PublishToWiki/PublishToWikiTask/PublishToWiki.ps1
@@ -1,14 +1,9 @@
 [CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true, HelpMessage="The path from the root wiki page to where the new page should be located")]
-    [string]$PathToFile,
+param ()
 
-    [Parameter(Mandatory=$true, HelpMessage="The URL of the Git repository that holds the project's wiki")]
-    [string]$WikiUri,
-
-    [Parameter(Mandatory=$true, HelpMessage="The markdown file to be uploaded to the project's wiki")]
-    [string]$File
-)
+$PathToFile = Get-VstsInput -Name PathToFile -Require
+$WikiUrl = Get-VstsInput -Name WikiUrl -Require
+$File = Get-VstsInput -Name File -Require
 
 # Set a flag to force verbose as a default
 $VerbosePreference = 'Continue'
@@ -22,10 +17,10 @@ $teamproject = $env:SYSTEM_TEAMPROJECT
 Write-Verbose "collectionUrl = [$collectionUrl]"
 Write-Verbose "teamproject = [$teamproject]"
 Write-Verbose "PathToFile = [$PathToFile]"
-Write-Verbose "WikiUri = [$WikiUri]"
+Write-Verbose "WikiUrl = [$WikiUrl]"
 Write-Verbose "File = [$File]"
 
-$localWikiPath = Get-WikiFromGit -Uri $WikiUri -TargetFolder "~\temp\pubWiki"
+$localWikiPath = Get-WikiFromGit -Uri $WikiUrl
 Write-Verbose "Cloning Wiki repository to $localWikiPath."
 
 $targetPath = "$localWikiPath\$PathToFile"
@@ -50,7 +45,7 @@ Write-Verbose "Added release note link ($releaseNote) to $releaseTOCFileName."
 Set-Location $localWikiPath
 git add .
 git commit -m "PublishToWiki task: Updated release notes in wiki"
-git push
+git -c http.extraheader="AUTHORIZATION: bearer $env:SYSTEM_ACCESSTOKEN" push
 Pop-Location
 Write-Verbose "Updated wiki in VSTS."
 
--- Extensions/PublishToWiki/PublishToWikiTask/PublishToWiki.psm1
+++ Extensions/PublishToWiki/PublishToWikiTask/PublishToWiki.psm1
@@ -1,22 +1,33 @@
 function Get-WikiFromGit {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)][string]$Uri,
-        [Parameter(Mandatory=$false)][string]$TargetFolder = '.'
+        [Parameter(Mandatory=$true)][string]$Uri
     )
+
+    $PublishToWikiTaskGuid = "cfbe4a47-dabf-45af-a0e4-978f6e86ec75"
+    $WikiFolder = "$env:BUILD_BINARIESDIRECTORY\$PublishToWikiTaskGuid\Wiki"
 
     # Save current location, so that we can return to it later
     
-    if (Test-Path "$TargetFolder\Wiki") {
-        Remove-Item -Path "$TargetFolder\Wiki" -Recurse -Force
+    if (Test-Path $WikiFolder) {
+        Remove-Item -Path $WikiFolder -Recurse -Force
     }
 
+    mkdir $WikiFolder
     Push-Location
-    Set-Location $TargetFolder
-    git clone $Uri Wiki
+    Set-Location $WikiFolder
+    
+    git init $WikiFolder
+    git remote add origin $Uri
+    git config gc.auto 0
+    git config --get-all http.$Uri.extraheader
+    git -c http.extraheader="AUTHORIZATION: bearer $env:SYSTEMACCESSTOKEN" fetch --tags --prune --progress --no recurse-submodules origin
+    git checkout --progress --force master
+    git config http.extraheader="AUTHORIZATION: bearer $env:SYSTEMACCESSTOKEN"
+
     Pop-Location
     
-    Write-Output "$TargetFolder\Wiki"
+    Write-Output $WikiFolder
 }
 
 function Get-FileName {
